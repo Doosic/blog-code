ac043cbe769cd8636569061a746f233cc35b8e3b
doosic <strou73@naver.com>
2024-11-22 10:39:43 +0900

    first init

---
+++ aop-custom-annotation/src/main/java/com/doosic/aopcustomannotation/AopCustomAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.doosic.aopcustomannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopCustomAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopCustomAnnotationApplication.class, args);
+    }
+
+}
